aefed9cc910c1d0f4d5d90d7e17ce40e7dea8415
Justin Clifford Smith <justin.clifford.smith@gmail.com>
2021-12-08 09:34:53 -0800

    Minor clean up.

--- day6/day6_final.py
+++ day6/day6_final.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 with open("input6.txt", "r") as f:
     lines = f.readlines()
 
@@ -8,7 +6,7 @@ starting_state = lines[0].strip().split(",")
 state = [int(x) for x in starting_state]
 print("Initial state:", state)
 
-# Change this for day 1 (80 days) or day 2 (256 days).
+# Change this for part 1 (80 days) or part 2 (256 days).
 days = 256
 
 counter = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
@@ -16,7 +14,6 @@ for s in state:
     counter[s] += 1
 
 print(counter)
-
 
 for day in range(1, days + 1):
     starting_zero_count = counter[0]
